04b490dd61b6ca8302d9dbcec60d012b5b55da79
akalji <akalji@ya.ru>
2016-02-21 19:01:59 +0300

    Initial Commit

---
+++ cp_1_13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cp_1_13
+//
+//  Created by Nikolay Tikhonov on 21.02.16.
+//  Copyright © 2016 Nikolay Tikhonov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
